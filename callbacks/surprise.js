071d605d90a06633d6f35e3f60633e7092e0c9e8
Rose Cass <rose.cass12@gmail.com>
2017-10-21 18:27:59 -0700

    Alternative callback examples

--- callbacks/surprise.js
+++ callbacks/surprise.js
@@ -4,4 +4,27 @@ function showSurprise() {
     surpriseSection.textContent = '🎉 Surprise! 🎉';
 }
 
-setTimeout(showSurprise, 1000);
+// setTimeout(showSurprise, 1000);  // plain setTimeout
+
+/* const randomTime = Math.random() * 4000;
+
+setTimeout(showSurprise, randomTime); */
+
+
+/* Anonymous Function Callback
+
+const randomTime = Math.random() * 4000;
+
+setTimeout(function() {
+    surpriseSection.textContent = '🎉 Surprise! 🎉';
+}, randomTime); */
+
+
+/* Arrow Function Callback
+
+const randomTime = Math.random() * 4000;
+
+setTimeout(() => 
+    surpriseSection.textContent = '🎉 Surprise! 🎉' , randomTime); */
+
+    
